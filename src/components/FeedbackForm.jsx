03d0ff76797fff8c01379365bb26b17f633dd38a
joeabouserhal <joeabouserhal77777@gmail.com>
2022-06-30 11:51:11 +0300

    added submit button and text validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
-import Card from './Card'
+import React, { useState } from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
 
 const FeedbackForm = () => {
+
+  const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  // every time the text in the iput is changed
+  // we update the text state
+  const handleTextChange = (e) => {
+    if (text === '') {
+      // if the text is empty, the button is disabled
+      setBtnDisabled(true)
+      setMessage(null)
+      // if the text is not empty but shorter than 10 characters,
+      // then the button is disabled
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters long')
+      // else the message is > 10 characters long so we can submit
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
+    setText(e.target.value)
+  }
+
   return (
     <Card>
-        <form>
-            <h2>How would you rate</h2>
-            {/* TODO: Rating select bar */}
-            <div className="input-group">
-                <input type="text" placeholder='Enter Review'/>
-                <button type="submit">Submit</button>
-            </div>
-        </form>
+      <form>
+        <h2>How would you rate</h2>
+        {/* TODO: Rating select bar */}
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder='Enter Review'
+            onChange={handleTextChange}
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>Submit</Button>
+          {/* if there is a message, display it */}
+        </div>
+        {message && <p className='message'>{message}</p>}
+      </form>
     </Card>
   )
 }
